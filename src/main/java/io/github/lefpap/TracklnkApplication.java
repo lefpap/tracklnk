fd3e7cb9b7f77a0b9a318f32c3f6582c7f2482c6
lefpap <lolopap21@gmail.com>
2025-10-19 15:17:32 +0300

    Initial commit

---
+++ src/main/java/io/github/lefpap/TracklnkApplication.java
@@ -0,0 +1,13 @@
+package io.github.lefpap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracklnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracklnkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lefpap/TracklnkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lefpap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracklnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
